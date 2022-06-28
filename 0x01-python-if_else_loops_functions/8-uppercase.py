5e1c55ea608cd66ff3879e136bb8f3cb3f828610
Sgechuki <sgechuki@outlook.com>
2022-06-28 11:49:01 -0700

    a + b

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            asc = chr(ord(str[i]) - 32)
+            print("{}".format(asc))
+        else:
+            print("{}".format(str[i]))
