241a5697b1117e570d08d12e9e07c882dbfdb690
Sgechuki <sgechuki@outlook.com>
2022-06-30 11:35:13 -0700

    Fix output error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     import sys
 
     arg_no = len(sys.argv)
-    if arg_no == 0:
-        print("{} arguments.".format(arg_no))
-    elif arg_no == 1:
-        print("{} argument:".format(arg_no))
-        print("1: {}".format(sys.argv[0]))
+    if arg_no == 1:
+        print("{} arguments.".format(arg_no - 1))
+    elif arg_no == 2:
+        print("{} argument:".format(arg_no - 1))
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(arg_no))
-        for i in sys.argv:
+        print("{} arguments:".format(arg_no - 1))
+        for i in range(1, arg_no):
             print("{}: {}".format(i, sys.argv[i]))
