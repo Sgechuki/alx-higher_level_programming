5e4cf28a81bbaef77062462ade49a3e042abc2a1
Sgechuki <sgechuki@outlook.com>
2022-07-02 05:30:05 -0700

    Fix checker error 5

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range((len(my_list) - 1), -1, -1):
-        print("{:d}".format(my_list[i]))
+    i = 0
+    ind = -1
+    while i < len(my_list):
+        print("{:d}".format(my_list[ind]))
+        i = i + 1
+        ind = (-1 - 1)
