ca912a1f6680136fcff13aa30c7a6eba143966d7
Sgechuki <sgechuki@outlook.com>
2022-07-02 08:23:46 -0700

    Fix error 8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in range(0, len(row)):
-            if i < (len(row) - 1):
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                print("{:d}".format(row[i]))
+    if matrix:
+        for row in matrix:
+            for i in range(0, len(row)):
+                if i < (len(row) - 1):
+                    print("{:d}".format(row[i]), end=" ")
+                else:
+                    print("{:d}".format(row[i]))
