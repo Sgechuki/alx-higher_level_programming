fc1682e75088b607a20c3121c7b32453d4c19e97
Sgechuki <sgechuki@outlook.com>
2022-07-03 12:57:53 -0700

    eureka

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-    if len_a >= 2 and len_b >= 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len_a >= 2 and len_b == 1:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1])
-    elif len_a == 1 and len_b >= 2:
-        return (tuple_b[0] + tuple_a[0], tuple_b[1])
-    elif len_a == 0:
-        if len_b > 1:
-            return (tuple_b[0],  tuple_b[1])
-        elif len_b == 1:
-            return (tuple_b[0], 0)
-    elif len_b == 0:
-        if len_a > 1:
-            return (tuple_a[0],  tuple_a[1])
-        elif len_a == 1:
-            return (tuple_a[0], 0)
-    else:
-        return ()
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
