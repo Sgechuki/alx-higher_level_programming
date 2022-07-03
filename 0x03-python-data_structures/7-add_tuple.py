acc3e28496077b2972f92572a9b22789f7cffd14
Sgechuki <sgechuki@outlook.com>
2022-07-03 12:25:06 -0700

    Correct checker errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,8 +9,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len_a == 1 and len_b >= 2:
         return (tuple_b[0] + tuple_a[0], tuple_b[1])
     elif len_a == 0:
-        return tuple_b
+        if len_b > 1:
+            return (tuple_b[0],  tuple_b[1])
+        elif len_b == 1:
+            return (tuple_b[0], 0)
     elif len_b == 0:
-        return tuple_a
+        if len_a > 1:
+            return (tuple_a[0],  tuple_a[1])
+        elif len_a == 1:
+            return (tuple_a[0], 0)
     else:
         return (0, 0)
