1abf6cb65f2d388280aa4bc69f63d0cab6b4a8b7
Sgechuki <sgechuki@outlook.com>
2022-07-19 12:17:33 -0700

    Print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module creates class Square
+optional private instance attribute size and
+Public instance method area"""
+
+
+class Square:
+    """This class defines a square
+        Attrinutes:
+        size (int, optional):
+        Must be integer, otherwise raise a TypeError exception.
+        If size is less than 0, raise a ValueError exception"""
+    def __init__(self, size=0):
+        """Initializes object."""
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrieves size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Does the validation before setting size attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
