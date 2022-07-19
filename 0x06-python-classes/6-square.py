926ee9128652479a1c3144324810ce9acb1f4858
Sgechuki <sgechuki@outlook.com>
2022-07-19 13:31:39 -0700

    Dont fill

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                if self.__position[0] > 0:
+                if self.__position[0] > 0 and self.__position[1] == 0:
                     print(" " * self.__position[0], end="")
                 for j in range(self.__size):
                     print("#", end="")
