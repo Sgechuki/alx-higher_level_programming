c8e9bdfa5e12cdeca20ac34b92a4f2fb545222dd
Sgechuki <sgechuki@outlook.com>
2022-07-19 13:40:05 -0700

    Add spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,8 +58,10 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for k in range(self.__position[1]):
+                print()
             for i in range(self.__size):
-                if self.__position[0] > 0 and self.__position[1] == 0:
+                if self.__position[0] > 0:
                     print(" " * self.__position[0], end="")
                 for j in range(self.__size):
                     print("#", end="")
