ba1cb4d3eb7b336377f1e3170739c6a87adad370
Sgechuki <sgechuki@outlook.com>
2022-08-01 12:34:17 -0700

    Fix req violation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""This module holds the class Rectangle"""
+"""This module holds class BaseGeometry"""
 
+
+class BaseGeometry(object):
+    """Inherits from object class"""
+    def area(self):
+        """raises exception error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """that validates value"""
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """Inherits from the BaseGeometry class"""
