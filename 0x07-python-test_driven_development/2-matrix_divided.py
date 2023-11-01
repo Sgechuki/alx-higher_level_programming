b2cd8c6f22bdaf09f12ad255df11eacd02d036d6
Sgechuki <sgechuki@outlook.com>
2023-11-01 11:52:05 -0700

    Matrix divided control flow

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,22 +8,15 @@
 
 def matrix_divided(matrix, div):
     """divides all elements of a matrix"""
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
     m = "matrix must be a matrix (list of lists) of integers/floats"
-    if not isinstance(matrix, list):
-        raise TypeError(m)
     if not all(isinstance(x, list) for x in matrix):
         raise TypeError(m)
-    iof = all(isinstance(y, (int, float)) for x in matrix for y in x)
-    if not iof:
+    elif not all(isinstance(y, (int, float)) for x in matrix for y in x):
         raise TypeError(m)
-
-    size = len(matrix[0])
-    sl = all(size == len(x) for x in matrix)
-    if not sl:
+    elif not all(len(matrix[0] == len(x) for x in matrix)):
         raise TypeError("Each row of the matrix must have the same size")
-
+    elif not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
     return [[round(column / div, 2) for column in row] for row in matrix]
