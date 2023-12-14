1bbc08a7d6a29f61a53f8eb6e0de964885985c30
Sgechuki <sgechuki@outlook.com>
2023-12-14 11:10:20 -0800

    Task 4-5

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- 3 always a name
+-- creates the table force_name
+CREATE TABLE IF NOT EXISTS force_name
+(
+	id INT,
+	name VARCHAR(256)
+)
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- 5 Unique ID
+-- creates the table unique_id
+CREATE TABLE IF NOT EXISTS unique_id
+(
+	id INT UNIQUE DEFAULT 1,
+	name VARCHAR(256)
+);
