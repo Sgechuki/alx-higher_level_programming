f2ba317c35f16f9aced467ad728250638200adcf
Sgechuki <sgechuki@outlook.com>
2023-12-14 11:37:27 -0800

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (
 	id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY(cities_states_fk) REFERENCES states(id),
+	FOREIGN KEY(cities_states_fk) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 );
