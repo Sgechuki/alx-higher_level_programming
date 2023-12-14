851792582d67faf0a2aa3a8ec82429a818aa4641
Sgechuki <sgechuki@outlook.com>
2023-12-14 11:31:11 -0800

    Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- 7 Cities table
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(
+	id INT AUTO_GENERATED PRIMARY KEY UNIQUE NOT NULL,
+	state_id INT NOT NULL FOREIGN KEY(cities_states_fk) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
+)
