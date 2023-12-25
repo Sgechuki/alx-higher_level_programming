9d7395b0b2d1920d3ce1d5db3ed4c818ec0a34c6
Sgechuki <sgechuki@outlook.com>
2023-12-25 08:27:35 -0800

    Convert db table and colimn to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,18 @@
+-- convert all of the following to UTF8
+-- Database hbtn_0c_0
+-- Table first_table
+-- Field name in first_table
+ALTER DATABASE
+hbtn_0c_0
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE
+first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+ALTER TABLE
+first_table
+CHANGE name
+VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
