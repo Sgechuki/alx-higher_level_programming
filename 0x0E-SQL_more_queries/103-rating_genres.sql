2facf4ba24189d019e9f4c4aae06861bd7d98cc7
Seth <64912271+Sgechuki@users.noreply.github.com>
2023-12-28 22:31:18 +0300

    Change rating_sum to rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- 20. Best genre
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT name, SUM(rate) rating_sum
+SELECT name, SUM(rate) rating
 FROM tv_genres tg
 INNER JOIN tv_show_genres tsg ON tsg.genre_id = tg.id
 INNER JOIN tv_show_ratings tsr ON tsr.show_id = tsg.show_id
