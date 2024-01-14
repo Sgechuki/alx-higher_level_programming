108444cf4b6232149e6f6cb067e79c0303469e26
Sgechuki <sgechuki@outlook.com>
2024-01-14 11:20:52 -0800

    Task 2: recorrection

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,8 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY 1".format(argv[4])
+    query = "SELECT * FROM states WHERE name = BINARY\
+            '{}' ORDER BY 1".format(argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
