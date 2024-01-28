7ceca656af1360c86a34cfc0f2df93ed5d704bf3
Sgechuki <sgechuki@outlook.com>
2024-01-28 10:50:46 -0800

    Add the if __name__ == __main__

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,7 +11,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-url = "https://api.github.com/user"
-r = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
-response = r.json()
-print(response.get("id"))
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    response = r.json()
+    print(response.get("id"))
